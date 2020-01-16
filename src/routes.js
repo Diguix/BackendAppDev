8edfc65ed183e8d5e9e5e22424e2ef34d7d2f7bd
Diguix <rtteixeira1@gmail.com>
2020-01-16 06:35:18 -0300

    refactory routes

--- src/routes.js
+++ src/routes.js
@@ -1,51 +1,13 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Devs');
-const arrayAsString = require('./utils/parseStringAsArray');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
 const routes = Router();
 
-routes.post('/devs', async (req, resp) => {
-    try {
-        const { github_username, techs, latitude, longitude } = req.body;
-
-        // let resFindOne = await axios.findOne({ github_username }, () => {
-        //     try {
-        //         console.info(' ===> ', resFindOne); // validando
-        //     } catch (error) {}
-        // });
-
-        const apiResp = await axios.get(
-            `https://api.github.com/users/${github_username}`
-        );
-
-        const { name = login, avatar_url, bio } = apiResp.data;
-
-        // console.log(apiResp); // validando
-
-        const techArray = arrayAsString(techs); // irá remove caracter vazio.
-
-        console.info(' techArray ===>', techArray);
-
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-        };
-
-        console.info(' location ===>', location);
-
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techArray,
-            location,
-        });
-
-        return resp.json(dev);
-    } catch (e) {
-        console.info('Error no ', e);
-    }
-});
+routes.post('/devs/store', DevController.store);
+routes.get('/devs/list', DevController.index);
+routes.get('/devs/search', SearchController.index);
+routes.delete('/devs/delete/:id', DevController.destroy);
+routes.put('/devs/update/:id', DevController.update);
 
 module.exports = routes;
