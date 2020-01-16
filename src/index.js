64a20c4f23278f29f08b4b35b68eb78f9d251dbd
Diguix <rtteixeira1@gmail.com>
2020-01-16 06:33:48 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const requireDir = require('require-dir');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+const dev = require('./controllers/DevController');
+const search = require('./controllers/SearchController');
+
 const app = express();
 
-app.use(express.json()); // para que o express() entenda as requisições no formato json
-
+// Para usar banco local usar NODE_ENV como local
 const mongoUri =
     process.env.NODE_ENV === 'local'
         ? process.env.MONGO_URL_LOCAL
@@ -15,31 +19,29 @@ const port = process.env.PORT || 3333;
 const host = process.env.HOST || 'localhost';
 const url = `http://${port}:${host}`;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose
     .connect(mongoUri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
         useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     })
-    .then(() => console.log('Conexão OK com o MongoDB...'))
-    .catch(e => console.error(e));
+    .then(() => console.log('Conexão estabelecida com o MongoDB...'))
+    .catch(err => console.error(err));
 
-/**
- *
- * Metodos HTTP: GET, POST, PUT, DELETE
- *
- */
+// App
+app.use(express.json()); // para que o express() entenda as requisições no formato json
+app.use(express.urlencoded({ extended: true })); // permite lidar com requisicoes no padrao urlenconded
 
-/**
- *  Tipos de parametros de requisições
- *  Query Params: req.params (filtro, ordenação, paginação)
- *  Route Params: identificar um recurso em alteracao ou remocao
- *  Body
- */
-
+// app.use('/', dev, search);
 app.use(routes);
 
 app.listen(port, host, () => {
-    console.info(`Conectado na porta  ${port} `),
-        console.info(`Conectado no mongoDB  ${url} `);
+    console.log(`Servidor executando em  ${url} `),
+        console.log(`Servidor executando na uri  ${mongoUri} `);
 });
