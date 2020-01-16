ded6a63c73fca05e36fb18bbaea5784036380bc1
Diguix <rtteixeira1@gmail.com>
2020-01-16 06:34:46 -0300

    refactory models

--- src/models/Devs.js
+++ src/models/Devs.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const PointSchema = require('./utils/PointSchema')
 
 const DevSchema = new mongoose.Schema({
+    id: String,
     name: String,
     github_username: String,
     bio: String,
     avatar_url: String,
-    techs: [String],
+    techs: {type: [String], uppercase: true},
     location: {
         type: PointSchema,
         index: '2dsphere'
